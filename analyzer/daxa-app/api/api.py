38cad2b2dc9a31867b6b10391b37fe7931cfd233
shreyas.cd <shreyas@clouddefense.io>
2024-01-16 13:00:24 +0530

    - Made .daxa as cache dir for report & metadata
    - Replaced run_id with load_id
    - Updated path as /app/discover
    
    - Removed unwanted files and code.
    
    - Changes after testing.

--- analyzer/daxa-app/api/api.py
+++ analyzer/daxa-app/api/api.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2024 Daxa. All rights reserved.
 from fastapi import APIRouter
 from service.service import AppDiscover, AppLoaderDoc
-from utils.utils import get_run_id
 
 
 class App:
@@ -13,16 +12,14 @@ class App:
 
     @staticmethod
     def discover(data: dict):
-        # "/discover" API Entrypoint
-        # Get run id
-        run_id = get_run_id()
-        discovery_obj = AppDiscover(data=data, run_id=run_id)
+        # "/app/discover" API entrypoint
+        discovery_obj = AppDiscover(data=data)
         response = discovery_obj.process_request()
         return response
 
     @staticmethod
     def loader_doc(data: dict):
-        # "/loader/doc"
+        # "/loader/doc" API entrypoint
         loader_doc_obj = AppLoaderDoc(data=data)
         response = loader_doc_obj.process_request()
         return response
--- analyzer/daxa-app/service/service.py
+++ analyzer/daxa-app/service/service.py
@@ -1,18 +1,17 @@
 # Copyright (c) 2024 Daxa. All rights reserved.
 
-from datetime import datetime, timezone
-
-from models.models import Chain, LoaderMetadata, Metadata, VectorDb, PackageInfo, AiDataModel, AiDocs, AiApp
+from datetime import datetime
 from entity_classifier.entity_classifier import EntityClassifier
+from enums.enums import CacheDir
 from utils.utils import write_json_to_file, read_json_file
-
 from libs.logger import logger
+from models.models import LoaderMetadata, Metadata, AiDataModel, AiDocs, AiApp
 
 
 class AppDiscover:
-    def __init__(self, data: dict, run_id: str):
+    def __init__(self, data: dict):
         self.data = data
-        self.run_id = run_id
+        self.load_id = data.get('load_id')
 
     def process_request(self):
         """
@@ -20,50 +19,14 @@ class AppDiscover:
             """
         try:
             application_name = self.data.get("name")
+            owner = self.data.get("owner")
             logger.debug(f"AI_APP [{application_name}]: Input Data: {self.data}")
 
             # Writing app metadata file
-            file_context = {"name": application_name, "current_run_id": self.run_id}
-            file_path = f"{application_name}/metadata/metadata.json"
+            file_context = {"name": application_name, "current_load_id": self.load_id}
+            file_path = f"{CacheDir.home_dir.value}/{application_name}/{CacheDir.metadata_file_path.value}"
             write_json_to_file(file_context, file_path)
-            # get chain details: Not present in OpenSource Input
-            # chains = list()
-            # for chain in self.data.get('chains', []):
-            #     name = chain["name"]
-            #     model = chain['model']
-            #     # vector db details
-            #     vector_db_details = []
-            #     for vector_db in chain.get('vector_dbs', []):
-            #         vector_db_obj = VectorDb(name=vector_db.get("name"),
-            #                                  version=vector_db.get("version"),
-            #                                  location=vector_db.get("location"),
-            #                                  embeddingModel=vector_db.get("embedding_model"))
-            #
-            #         package_info = vector_db.get("pkg_info", {})
-            #         if package_info:
-            #             pkg_info_obj = PackageInfo(projectHomePage=package_info.get("project_home_page"),
-            #                                        documentationUrl=package_info.get("documentation_url"),
-            #                                        pypiUrl=package_info.get("pypi_url"),
-            #                                        licenceType=package_info.get("liscence_type"),
-            #                                        installedVia=package_info.get("installed_via"),
-            #                                        location=package_info.get("location"))
-            #             vector_db_obj.packageInfo = pkg_info_obj.dict()
-            #
-            #         vector_db_details.append(vector_db_obj.dict())
-            #     chain_obj = Chain(name=name, model=model, vectorDbs=vector_db_details)
-            #     chains.append(chain_obj.dict())
 
-            # get loader details: Not present in OpenSource Input
-            # loaders = list()
-            # for loader in self.data.get('loaders', []):
-            #     loader_obj = LoaderMetadata(name=loader.get('loader'),
-            #                                 sourcePath=loader.get('source_path'),
-            #                                 sourceType=loader.get('source_type'))
-            #     loaders.append(loader_obj.dict())
-
-            # logger.debug(f"AI_APPS [{application_name}]: Loaders: {loaders}")
-
-            # instance_dict = self.data.get("instance", {}): Not present in OpenSource Input
             runtime_dict = self.data.get("runtime", {})
             instance_details = {
                 "language": runtime_dict.get("language"),
@@ -82,135 +45,38 @@ class AppDiscover:
             }
             logger.debug(f"AI_APPS [{application_name}]: Instance Details: {instance_details}")
 
-            last_used = datetime.now(timezone.utc)
+            last_used = datetime.now()
 
             metadata = Metadata(
-                createdAt=datetime.utcnow(),
-                modifiedAt=datetime.utcnow()
+                createdAt=datetime.now(),
+                modifiedAt=datetime.now()
             )
 
             ai_app = {
                 "metadata": metadata,
                 "name": application_name,
+                "owner": owner,
                 "pluginVersion": self.data.get("plugin_version"),
                 "instanceDetails": instance_details,
-                # "chains": chains,
-                # "loaders": loaders,
                 "framework": self.data.get("framework"),
                 "lastUsed": last_used
             }
             ai_app_obj = AiApp(**ai_app)
             logger.debug(f"Final Output For Discovery Call: {ai_app_obj.dict()}")
-            file_path = f"{application_name}/{self.run_id}/report.json"
+            file_path = f"{CacheDir.home_dir.value}/{application_name}/{self.load_id}/{CacheDir.report_file_name.value}"
             write_json_to_file(ai_app_obj.dict(), file_path)
-            return ai_app_obj
+            # return {"Request Processed Successfully"}
+            # TODO: remove below line after testing.
+            return ai_app_obj.dict()
         except Exception as ex:
             response = f"Error in process_request. Error:{ex}"
             logger.error(response)
             return response
 
 
-class AppLoaderDoc():
+class AppLoaderDoc:
     def __init__(self, data):
         self.data = data
-
-    def process_request(self):
-        """This process is entrypoint function for loader doc API implementation."""
-        logger.debug(f"Loader Doc, Input Data: {self.data}")
-
-        try:
-            app_name = self.data.get("name")
-            logger.debug(f"AI Loader Doc, AppName: {app_name}")
-
-            # Read metadata file & get run details
-            metadata_file_path = f"{app_name}/metadata/metadata.json"
-            app_metadata = read_json_file(metadata_file_path)
-            if not app_metadata:
-                return {"Message": "App details not present, Please call discovery api first"}
-            run_id = app_metadata.get("current_run_id")
-
-            # get current app details from run id
-            run_file_path = f"{app_name}/{run_id}/report.json"
-            app_details = read_json_file(run_file_path)
-
-            # Creating AiApps Objects
-            metadata_obj = Metadata(
-                    createdAt=datetime.utcnow(),
-                    modifiedAt=datetime.utcnow()
-                )
-            last_used = datetime.now(timezone.utc)
-
-            # Get Loader Details
-            loader_details = self.data.get("loader_details", {})
-            loader_name = loader_details.get('loader', None)
-            source_type = loader_details.get('source_type', None)
-            source_path = loader_details.get('source_path', None)
-
-            if loader_name and source_type:
-                loader_list = app_details.get('loaders', [])
-                loader_exist = False
-                for loader in loader_list:
-                    # If loader exist, updating loader SourcePath and SourceType
-                    if loader and loader.get('name', "") == loader_name:
-                        loader['sourcePath'] = source_path
-                        loader['sourceType'] = source_type
-                        loader_exist = True
-
-                # If loader does not exist
-                if not loader_exist:
-                    logger.debug("loader not exist in db")
-                    new_loader_data = LoaderMetadata(name=loader_name,
-                                                     sourcePath=source_path,
-                                                     sourceType=source_type)
-                    # Append New Loader Details
-                    loader_list.append(new_loader_data.dict())
-
-                # update loader details in app details
-                app_details["loader_details"] = loader_list
-
-            doc_list = self.data.get('docs', [])
-            doc_report = []
-            for doc in doc_list:
-                """ Get Classifier Response From Presidio"""
-                doc_info: AiDataModel = self._get_classifier_response(doc)
-                if doc:
-                    # Creating Entry for Ai Docs Collection
-                    loader_docs_data = AiDocs(name=self.data.get('name'),
-                                              appId=run_id,
-                                              metadata=metadata_obj,
-                                              doc=doc.get('doc'),
-                                              sourcePath=doc.get('source_path'),
-                                              loaderSourcePath=source_path,
-                                              lastModified=last_used,
-                                              entityCount=doc_info.entityCount,
-                                              entities=doc_info.entities,
-                                              topicCount=doc_info.topicCount,
-                                              topics=doc_info.topics
-                                              )
-                    """ Create doc report """
-                    doc_report.append(loader_docs_data.dict())
-
-                    """Updating loader Source file in AI APP collection """
-                    # run_with_transaction(self.loader_source_file_update_tx, self.db, ai_app_db_output, source_path, doc)
-
-                    """Check policy violations for loader doc {Need to Discuss with shreyas }"""
-                    # check_doc_policy_violations(self.db, ai_app_db_output, ai_docs_obj.dict())
-                    # res_doc_obj = ai_docs_obj.dict()
-                    # self.agg_doc_to_app(res_doc_obj.get('entities', {}), res_doc_obj.get('topics', {}),
-                    #                     res_doc_obj.get('id'))
-            logger.debug(f"Loader Doc Details: {doc_report}")
-            app_details["doc_report"] = doc_report
-
-            logger.debug(f"Final Report with doc details: {app_details}")
-
-            # This write will overwrite app_discovery Report
-            file_path = f"{app_name}/{run_id}/report.json"
-            write_json_to_file(app_details, file_path)
-            return { "Loader Doc Response": doc_report }
-
-        except Exception as ex:
-            print(f"AI_LOADER_DOC Failed: Error in process_request. Error:{ex}")
-            return {"message": f"Error: {ex}"}
 
     @staticmethod
     def _get_classifier_response(doc):
@@ -233,3 +99,92 @@ class AppLoaderDoc():
         except Exception as e:
             logger.error(f"Get Classifier Response Failed, Exception: {e}")
             return doc_info
+
+    def process_request(self):
+        """This process is entrypoint function for loader doc API implementation."""
+        logger.debug(f"Loader Doc, Input Data: {self.data}")
+
+        try:
+            app_name = self.data.get("name")
+            logger.debug(f"AI Loader Doc, AppName: {app_name}")
+
+            # Read metadata file & get current load details
+            app_metadata_file_path = f"{CacheDir.home_dir.value}/{app_name}/{CacheDir.metadata_file_path.value}"
+            app_metadata = read_json_file(app_metadata_file_path)
+            if not app_metadata:
+                return {"Message": "App details not present, Please call discovery api first"}
+            load_id = app_metadata.get("current_load_id")
+
+            # Get current app details from load id
+            run_file_path = f"{CacheDir.home_dir.value}/{app_name}/{load_id}/{CacheDir.report_file_name.value}"
+            app_details = read_json_file(run_file_path)
+
+            # Create Apps Object
+            metadata_obj = Metadata(
+                    createdAt=datetime.now(),
+                    modifiedAt=datetime.now()
+                )
+            last_used = datetime.now()
+
+            # Get Loader Details
+            loader_details = self.data.get("loader_details", {})
+            loader_name = loader_details.get('loader', None)
+            source_type = loader_details.get('source_type', None)
+            source_path = loader_details.get('source_path', None)
+
+            if loader_name and source_type:
+                loader_list = app_details.get('loaders', [])
+                loader_exist = False
+                for loader in loader_list:
+                    # If loader exist, update loader SourcePath and SourceType
+                    if loader and loader.get('name', "") == loader_name:
+                        loader['sourcePath'] = source_path
+                        loader['sourceType'] = source_type
+                        loader['lastModified'] = last_used
+                        loader_exist = True
+
+                # If loader does not exist, create new entry
+                if not loader_exist:
+                    logger.debug("loader not exist in db")
+                    new_loader_data = LoaderMetadata(name=loader_name,
+                                                     sourcePath=source_path,
+                                                     sourceType=source_type,
+                                                     lastModified=last_used)
+                    loader_list.append(new_loader_data.dict())
+
+            doc_list = self.data.get('docs', [])
+            docs = []
+            for doc in doc_list:
+                # Get Classifier Response
+                doc_info: AiDataModel = self._get_classifier_response(doc)
+                if doc:
+                    loader_docs_data = AiDocs(
+                                              appId=load_id,
+                                              metadata=metadata_obj,
+                                              doc=doc.get('doc'),
+                                              sourcePath=doc.get('source_path'),
+                                              loaderSourcePath=source_path,
+                                              lastModified=last_used,
+                                              entityCount=doc_info.entityCount,
+                                              entities=doc_info.entities,
+                                              topicCount=doc_info.topicCount,
+                                              topics=doc_info.topics
+                                              )
+                    docs.append(loader_docs_data.dict())
+
+            logger.debug(f"Loader Doc Details: {docs}")
+            app_details["docs"] = docs
+
+            # Update modifiedAt & lastUsed timestamp
+            app_details["metadata"]["modifiedAt"] = last_used
+            app_details["lastUsed"] = last_used
+            logger.debug(f"Final Report with doc details: {app_details}")
+
+            # This write will overwrite app_discovery Report
+            file_path = f"{CacheDir.home_dir.value}/{app_name}/{load_id}/{CacheDir.report_file_name.value}"
+            write_json_to_file(app_details, file_path)
+            return {"Loader Doc Response": docs}
+
+        except Exception as ex:
+            print(f"AI_LOADER_DOC Failed: Error in process_request. Error:{ex}")
+            return {"message": f"Error: {ex}"}
