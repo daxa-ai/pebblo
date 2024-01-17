cf7a141c5fbe427433354ca5ce6577fcca2f4a29
Nitin Kumar <nitinkumardeshmukh19@gmail.com>
2024-01-17 18:38:33 +0530

    feat: template images fixed

--- analyzer/reports/html_to_pdf_generator/report_generator.py
+++ analyzer/reports/html_to_pdf_generator/report_generator.py
@@ -1,6 +1,7 @@
 from weasyprint import HTML, CSS
 import jinja2
 import datetime
+import os
 
 # Convert HTML Template to PDF by embedding JSON data
 def convertHtmlToPdf(data, outputPath, templateName):
@@ -8,5 +9,6 @@ def convertHtmlToPdf(data, outputPath, templateName):
     templateEnv = jinja2.Environment(loader=templateLoader)
     template = templateEnv.get_template(templateName)
     sourceHtml = template.render(data=data, date=datetime.datetime.now()) 
-    htmldoc = HTML(string=sourceHtml, base_url="")
+    base_url = os.path.dirname(os.path.realpath(__file__))
+    htmldoc = HTML(string=sourceHtml, base_url=base_url)
     return  htmldoc.write_pdf(target=outputPath, stylesheets=[CSS('reports/templates/index.css')])
