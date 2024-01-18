5baa18578a7991fa2f953818969ca02cd66cb3d3
Rajendra Kadam <raj.725@outlook.com>
2024-01-18 10:54:20 +0530

    Delete commented code

--- analyzer/topic_classifier/config.py
+++ analyzer/topic_classifier/config.py
@@ -1,13 +1,6 @@
+# Confidence score
 TOPIC_CONFIDENCE_SCORE = 0.60
+
+# Model paths
 TOKENIZER_PATH = "daxa-ai/topic-classifier-rc1"
 CLASSIFIER_PATH = "daxa-ai/topic-classifier-rc1"
-
-# from enum import Enum
-
-# class ConfidenceScore(Enum):
-#     Topic = "0.60"
-#
-#
-# class ModelPath(Enum):
-#     TOKENIZER = "daxa-ai/topic-classifier-rc1"
-#     CLASSIFIER = "daxa-ai/topic-classifier-rc1"
