2792df1aa89b9103bd1bf7c1c2c3e95150207573
Rajendra Kadam <raj.725@outlook.com>
2024-01-25 18:38:02 +0530

    #691 Add commit-id support for pebblo-classifier model

--- pebblo/topic_classifier/config.py
+++ pebblo/topic_classifier/config.py
@@ -4,3 +4,7 @@ TOPIC_CONFIDENCE_SCORE = 0.60
 # Model paths
 TOKENIZER_PATH = 'daxa-ai/pebblo-classifier'
 CLASSIFIER_PATH = 'daxa-ai/pebblo-classifier'
+
+# Specific model version to use. Revision can be any identifier allowed by
+# git e.g. branch name, a tag name, or a commit id
+MODEL_REVISION = '708bbb3f2fe71b14e9c2e43f8100ccc31143bd0a'
