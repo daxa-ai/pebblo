c7c42ce3e420ab93f4f03bee4300f2c79cecdf54
Rajendra Kadam <raj.725@outlook.com>
2024-02-02 18:36:50 +0530

    Unit test for /loader/doc endpoint

--- tests/app/test_daemon.py
+++ tests/app/test_daemon.py
@@ -32,6 +32,32 @@ def entity_classifier():
         yield entity_classifier
 
 
+# DocHelper
+@pytest.fixture(scope="module")
+def doc_helper():
+    with patch('pebblo.app.service.service.DocHelper') as doc_helper:
+        yield doc_helper
+
+
+# Reports
+@pytest.fixture(scope="module")
+def reports():
+    with patch('pebblo.app.service.service.Reports') as reports:
+        yield reports
+
+
+@pytest.fixture
+def mock_read_json_file():
+    with patch('pebblo.app.service.service.read_json_file') as mock_read_json_file:
+        yield mock_read_json_file
+
+
+@pytest.fixture
+def mock_write_json_to_file():
+    with patch('pebblo.app.service.service.write_json_to_file') as mock_write_json_to_file:
+        yield mock_write_json_to_file
+
+
 def test_topic_classifier(topic_classifier):
     assert topic_classifier is not None
 
@@ -49,7 +75,6 @@ def test_root_endpoint():
     assert response.json() == {"detail": "Not Found"}
 
 
-@patch('pebblo.app.service.service.write_json_to_file', return_value=Mock())
 def test_app_discover_success(mock_write_json_to_file):
     """
     Test the app discover endpoint.
@@ -68,7 +93,6 @@ def test_app_discover_success(mock_write_json_to_file):
     assert response.json() == {"message": "App Discover Request Processed Successfully"}
 
 
-@patch('pebblo.app.service.service.write_json_to_file', return_value=Mock())
 def test_app_discover_validation_errors(mock_write_json_to_file):
     """
     Test the app discover endpoint with validation errors.
@@ -85,7 +109,6 @@ def test_app_discover_validation_errors(mock_write_json_to_file):
     assert "name\n  none is not an allowed value (type=type_error.none.not_allowed)" in detail
 
 
-@patch('pebblo.app.service.service.write_json_to_file', return_value=Mock())
 def test_app_discover_server_error(mock_write_json_to_file):
     """
     Test the app discover endpoint with server error.
@@ -102,3 +125,47 @@ def test_app_discover_server_error(mock_write_json_to_file):
     # Assertions
     assert response.status_code == 500
     assert response.json() == {"detail": "Internal Server Error"}
+
+
+def test_loader_doc_success(mock_write_json_to_file, mock_read_json_file, doc_helper, reports):
+    """
+    Test the loader doc endpoint success.
+    """
+    mock_write_json_to_file.return_value = None
+    mock_read_json_file.return_value = {"app": "Test App"}
+
+    doc_helper_instance = doc_helper.return_value
+    doc_helper_instance.process_docs_and_generate_report = Mock()
+    doc_helper_instance.process_docs_and_generate_report.return_value = ({"details": 1}, {"final_report": 1})
+
+    reports_instance = reports.return_value
+    reports_instance.generate_report = Mock()
+    reports_instance.generate_report.return_value = None
+
+    loader_doc = {
+        'name': 'Test App',
+        'owner': 'User',
+        'source_owner': 'User',
+        'load_id': '41fdd94e-f68b-4af1-b5d5-7243d2bb2ff6',
+        'loading_end': True,
+        'docs': [
+            {
+                'doc': 'Sample content',
+                'source_path': '/fake/path/topic_data.csv',
+                'last_modified': None,
+                'file_owner': 'User',
+                'source_path_size': 307
+            }
+        ],
+        'loader_details': {
+            'loader': 'CSVLoader',
+            'source_path': '/fake/path/topic_data.csv',
+            'source_type': 'file',
+            'source_path_size': 307,
+            'source_aggr_size': 306
+        },
+        'plugin_version': '0.1.0'
+    }
+    response = client.post("/v1/loader/doc", json=loader_doc)
+    assert response.status_code == 200
+    assert response.json() == {"message": "Loader Doc API Request processed successfully"}
