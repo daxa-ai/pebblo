484338eddb27daf9a68c1c1caf6b32a92c139aa6
kunal.cd <kunal@clouddefense.io>
2024-02-05 14:01:54 +0530

    Updated Test Cases

--- pebblo/app/daemon.py
+++ pebblo/app/daemon.py
@@ -1,22 +1,30 @@
-import uvicorn
-from fastapi import FastAPI
-from pebblo.app.routers.routers import router_instance
+from pebblo.app.config.config import load_config
+import sys
+import os
+import argparse
 
-from pebblo.topic_classifier.topic_classifier import TopicClassifier
-from pebblo.entity_classifier.entity_classifier import EntityClassifier
+
+config_details = {}
 
 
 def start():
+    global config_details
+    # For loading config file details
+    parser = argparse.ArgumentParser(description="Pebblo  CLI")
+    parser.add_argument('--config', type=str, help="Config file path")
+    args = parser.parse_args()
+    path = args.config
+    config_details = load_config(path)
+
+    # LazyLoading TopicClassifier and EntityClassifier to avoid circular dependency
+    from pebblo.topic_classifier.topic_classifier import TopicClassifier
+    from pebblo.entity_classifier.entity_classifier import EntityClassifier
     # Init TopicClassifier(This step downloads the models and put in cache)
     _ = TopicClassifier()
     # Init EntityClassifier(This step downloads all necessary training models)
-    _ = EntityClassifier()
+    _ = EntityClassifier
 
-    # Initialise app instance
-    app = FastAPI()
-
-    # Register the router instance with the main app
-    app.include_router(router_instance.router)
-
-    # running local server
-    uvicorn.run(app, host="localhost", port=8000, log_level="info")
+    # Starting Uvicorn Service Using config details
+    from pebblo.app.config.service import Service
+    svc = Service(config_details)
+    svc.start()
