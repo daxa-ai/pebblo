99b30f351d258a07547746cb8021e271b96b0cb1
kunal.cd <kunal@clouddefense.io>
2024-02-06 16:37:23 +0530

    Added config details

--- pebblo/app/config/config.py
+++ pebblo/app/config/config.py
@@ -36,33 +36,39 @@ class Config(BaseSettings):
 
 
 def load_config(path) -> Config:
-    # If Path does not exist in command, set default config value
-    if not path:
-        # Setting Default config details
-        conf_obj = Config(
-            daemon=PortConfig(
-                host='0.0.0.0',
-                port=3700
-            ),
-            reports=ReportConfig(
-                format='pdf',
-                outputDir='./config.py'
-            ),
-            logging=LoggingConfig(
-                level='info'
+    try:
+        # If Path does not exist in command, set default config value
+        if not path:
+            # Setting Default config details
+            conf_obj = Config(
+                daemon=PortConfig(
+                    host='0.0.0.0',
+                    port=3700
+                ),
+                reports=ReportConfig(
+                    format='pdf',
+                    outputDir='./config.py'
+                ),
+                logging=LoggingConfig(
+                    level='info'
+                )
             )
-        )
-        return conf_obj.dict()
+            return conf_obj.dict()
 
-    # If Path exist, set config value
-    else:
-        con_file = path
-        try:
-            with open(con_file, "r") as output:
-                cred_json = yaml.safe_load(output)
-                print(cred_json, Config)
-                parsed_config = Config.parse_obj(cred_json)
-                config_dict = parsed_config.dict()
-                return config_dict
-        except IOError as err:
-            print(f"no credentials file found at {con_file}")
+        # If Path exist, set config value
+        else:
+            con_file = path
+            try:
+                with open(con_file, "r") as output:
+                    cred_json = yaml.safe_load(output)
+                    print(cred_json, Config)
+                    parsed_config = Config.parse_obj(cred_json)
+                    config_dict = parsed_config.dict()
+                    return config_dict
+            except IOError as err:
+                print(f"no credentials file found at {con_file}")
+
+    except Exception as err:
+        print(f'Error while loading config details, err: {err}')
+
+
