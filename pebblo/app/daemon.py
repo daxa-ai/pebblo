09669823602be609d7b11908a00e40f643e39fc0
Kunal Jadhav <47965366+KunalJadhav5@users.noreply.github.com>
2024-02-07 17:13:47 +0530

    Updated cli code

--- pebblo/app/daemon.py
+++ pebblo/app/daemon.py
@@ -3,6 +3,7 @@ from pebblo.entity_classifier.entity_classifier import EntityClassifier
 
 from pebblo.app.config.config import load_config
 import sys
+import argparse
 
 from pebblo.app.config.service import Service
 
@@ -21,14 +22,13 @@ def start():
     global config_details
 
     # For loading config file details
-    if '--config' in cli_str:
-        path = cli_input[-1]
-        if path != '--config':
-            config_details = load_config(path)
-        else:
-            print('Please enter valid config path')
-    else:
-        config_details = load_config(None)
+    parser = argparse.ArgumentParser(description="Pebblo  CLI")
+    parser.add_argument('--config', type=str, help="Config file path")
+    args = parser.parse_args()
+    print(args)
+    path = args.config
+    print(path)
+    config_details = load_config(path)
 
     # Starting Uvicorn Service Using config details
     svc = Service(config_details)
