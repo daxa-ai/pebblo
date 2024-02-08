8e7706e69dd75686e6edf0ecbdb7250ad529a025
Shreyas Damle <57351398+shreyas-damle@users.noreply.github.com>
2024-02-08 13:48:57 +0530

    App History (#109)
    * App History code changes
    * Code refactoring for loader doc to make code more modular
    * Unit tests for loader doc
    ---------
    
    Co-authored-by: Rajendra Kadam <raj.725@outlook.com>
    Co-authored-by: shreyas.cd <shreyas@clouddefense.io>

--- pebblo/app/enums/enums.py
+++ pebblo/app/enums/enums.py
@@ -17,3 +17,4 @@ class CacheDir(Enum):
 class ReportConstants(Enum):
     snippets_limit = 100
     top_findings_limit = 5
+    loader_history_limit = 5
--- pebblo/app/models/models.py
+++ pebblo/app/models/models.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field
 from typing import Optional, List, Union
 from datetime import datetime
+from uuid import UUID
 
 
 class Metadata(BaseModel):
@@ -39,7 +40,6 @@ class AiDocs(BaseModel):
     entities: dict
     topicCount: int
     topics: dict
-    policyViolations: Optional[List[dict]] = []
 
 
 class FrameworkInfo(BaseModel):
@@ -77,7 +77,7 @@ class Summary(BaseModel):
     findingsEntities: int
     findingsTopics: int
     totalFiles: int
-    filesWithRestrictedData: int
+    filesWithFindings: int
     dataSources: int
     owner: str
     createdAt: datetime = datetime.now()
@@ -110,11 +110,20 @@ class DataSource(BaseModel):
     # snippets: Optional[List[Snippets]]
 
 
+class LoadHistory(BaseModel):
+    loadId: UUID
+    reportName: str
+    findings: int
+    filesWithFindings: int
+    generatedOn: datetime
+
+
 class ReportModel(BaseModel):
     name: str
     description: Optional[str]
     framework: Optional[FrameworkInfo] = Field(default_factory=FrameworkInfo)
     reportSummary: Optional[Summary]
+    loadHistory: Optional[List[LoadHistory]]
     topFindings: Optional[List[TopFindings]]
     instanceDetails: Optional[InstanceDetails]
     dataSources: Optional[List[DataSource]]
