d4ae09c0b75c03a3201edc5efcd0730e381f9bdc
rutujaac <chaudhari.rutuja99@gmail.com>
2024-02-09 18:16:19 +0530

    added logger

--- pebblo/reports/reports.py
+++ pebblo/reports/reports.py
@@ -2,6 +2,7 @@
 
 from pebblo.reports.html_to_pdf_generator.report_generator import convertHtmlToPdf;
 from pebblo.reports.enums.report_libraries import ReportLibraries, template_renderer_mapping
+from pebblo.reports.libs.logger import logger
 import os
 
 class Reports:
@@ -10,5 +11,10 @@ class Reports:
     def generate_report(data, outputPath='./report.pdf', format = 'pdf', renderer = ReportLibraries.WEASYPRINT):
         if format == 'pdf':
             searchPath = os.path.join(os.path.dirname(__file__), 'templates/')
-            templateName = template_renderer_mapping[renderer]
-            convertHtmlToPdf(data, outputPath, templateName = templateName, searchPath = searchPath, renderer = renderer)
+            try:
+                templateName = template_renderer_mapping[renderer]
+                convertHtmlToPdf(data, outputPath, templateName = templateName, searchPath = searchPath, renderer = renderer)
+            except Exception as e:
+                logger.error(f"Renderer {renderer} not supported. Please use supported renderers: {ReportLibraries.WEASYPRINT} or {ReportLibraries.XHTML2PDF}, {e}")
+        else:
+            logger.error("Output file format not supported")
