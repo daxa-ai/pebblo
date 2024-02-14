107f793aaad6888b7c798c3926364e496394c88d
Rahul Tripathi <rahul@clouddefense.io>
2024-02-14 13:54:16 +0530

    log further improvements.
    Signed-off-by: Rahul Tripathi <rahul@clouddefense.io>

--- pebblo/app/daemon.py
+++ pebblo/app/daemon.py
@@ -27,33 +27,33 @@ def start():
 
 def classifier_init():
     """Initialize topic and entity classifier."""
-    p_bar.write("Downloading models...")
+    p_bar.write("Downloading topic, entity classifier models ...")
     with redirect_stdout(StringIO()), redirect_stderr(StringIO()):
         from pebblo.topic_classifier.topic_classifier import TopicClassifier
         from pebblo.entity_classifier.entity_classifier import EntityClassifier
     p_bar.update(3)
-    p_bar.write("Topic Classifier Initializing.")
+    p_bar.write("Initializing topic classifier ...")
     p_bar.update(1)
    
     # Init TopicClassifier(This step downloads the models and put in cache)
     with redirect_stdout(StringIO()), redirect_stderr(StringIO()):
         _ = TopicClassifier()
-    p_bar.write("Topic Classifier Initialized...")
+    p_bar.write("Initializing topic classifier ... done")
     p_bar.update(1)
 
-    p_bar.write("Entity Classifier Initializing.")
+    p_bar.write("Initializing entity classifier ...")
     p_bar.update(1)
 
     # Init EntityClassifier(This step downloads all necessary training models)
     with redirect_stdout(StringIO()), redirect_stderr(StringIO()):
         _ = EntityClassifier()
-    p_bar.write("Entity Classifier Initialized...")
+    p_bar.write("Initializing entity classifier ... done")
     p_bar.update(1)
 
 
 def server_start(config_details):
     """Start server."""
-    p_bar.write("Pebblo server Starting.")
+    p_bar.write("Pebblo server starting ...")
     # Starting Uvicorn Service Using config details
     from pebblo.app.config.service import Service
     p_bar.update(1)
