0c1106f8c0bfa9bbf2ac733b803cc2bbdea93579
Shreyas Damle <57351398+shreyas-damle@users.noreply.github.com>
2024-02-15 12:19:15 +0530

    Skipped printing config details at the startup. (#159)
    Co-authored-by: shreyas.cd <shreyas@clouddefense.io>

--- pebblo/app/config/config.py
+++ pebblo/app/config/config.py
@@ -1,5 +1,4 @@
 import yaml
-from tqdm import tqdm
 
 from pydantic import BaseSettings, Field
 import pathlib
@@ -30,13 +29,6 @@ class Config(BaseSettings):
     reports: ReportConfig
     logging: LoggingConfig
 
-def print_config_output(config_output, p_bar=None):
-    if isinstance(p_bar, tqdm):
-        p_bar.write(config_output)
-    else:
-        print(config_output)
-
-
 def load_config(path, p_bar=None) -> Config:
     try:
         # If Path does not exist in command, set default config value
@@ -55,9 +47,6 @@ def load_config(path, p_bar=None) -> Config:
         )
         if not path:
             # Setting Default config details
-            config_details = f"Config values : {conf_obj.dict()}"
-            print_config_output(config_details, p_bar)
-
             return conf_obj.dict()
 
         # If Path exist, set config value
@@ -68,9 +57,6 @@ def load_config(path, p_bar=None) -> Config:
                     cred_json = yaml.safe_load(output)
                     parsed_config = Config.parse_obj(cred_json)
                     config_dict = parsed_config.dict()
-                    config_details = f"Config values : {config_dict}"
-                    print_config_output(config_details, p_bar)
-
                     return config_dict
             except IOError as err:
                 print(f"no credentials file found at {con_file}")
