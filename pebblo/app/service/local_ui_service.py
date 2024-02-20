7fede3e7316a5888690a9ea44274cbd462e1734c
Kunal Jadhav <47965366+KunalJadhav5@users.noreply.github.com>
2024-02-20 23:29:06 +0530

    Refactored local_service.py

--- pebblo/app/service/local_ui_service.py
+++ pebblo/app/service/local_ui_service.py
@@ -2,7 +2,7 @@ import json
 import os
 from pebblo.app.enums.enums import CacheDir
 from pebblo.app.libs.logger import logger
-from pebblo.app.utils.utils import get_full_path
+from pebblo.app.utils.utils import get_full_path, read_json_file
 
 
 def get_all_apps_list():
@@ -14,39 +14,30 @@ def get_all_apps_list():
     files_findings = 0
     data_source = 0
 
-    app_details = dict()
     for app_dir in dir_path:
         app_path = f'{CacheDir.home_dir.value}/{app_dir}/{CacheDir.metadata_file_path.value}'
         app_full_path = get_full_path(app_path)
-        try:
-            with open(app_full_path, "r") as output:
-                app_json = json.load(output)
-                app_details = dict()
-                app_details['name'] = app_json.get('name')
-                app_details['loadId'] = app_json.get('load_ids')[-1]
-                
-        except IOError as err:
-            logger.error(f"No  file found at {app_path}")
+        app_details = dict()
+        app_json = read_json_file(app_full_path)
+        app_details['name'] = app_json.get('name')
+        app_details['loadId'] = app_json.get('load_ids')[-1]
 
         app_detail_path = f'{CacheDir.home_dir.value}/{app_dir}/{app_json.get("load_ids")[-1]}/{CacheDir.report_data_file_name.value}'
-        print()
-        app_detail_full_path = get_full_path(app_detail_path)
-        try:
-            with open(app_detail_full_path, "r") as output:
-                app_detail_json = json.load(output)
-                report_summary = app_detail_json.get('reportSummary')
-                app_details['topics'] = report_summary.get('findingsTopics', 0)
-                app_details['entities'] = report_summary.get('findingsEntities', 0)
-                app_details['owner'] = report_summary.get('owner')
-                findings += report_summary.get('findings', 0)
-                files_findings += report_summary.get('filesWithFindings', 0)
-                data_source += report_summary.get('dataSources', 0)
-                if report_summary.get('findings', 0) > 0:
-                    app_risk += 1
-                app_details['loadId'] = app_json.get('load_ids')[-1]
 
-        except IOError as err:
-            logger.error(f"No  file found at {app_detail_path}")
+        app_detail_full_path = get_full_path(app_detail_path)
+
+        app_detail_json = read_json_file(app_detail_full_path)
+        report_summary = app_detail_json.get('reportSummary')
+        app_details['topics'] = report_summary.get('findingsTopics', 0)
+        app_details['entities'] = report_summary.get('findingsEntities', 0)
+        app_details['owner'] = report_summary.get('owner')
+        findings += report_summary.get('findings', 0)
+        files_findings += report_summary.get('filesWithFindings', 0)
+        data_source += report_summary.get('dataSources', 0)
+        if report_summary.get('findings', 0) > 0:
+            app_risk += 1
+        app_details['loadId'] = app_json.get('load_ids')[-1]
+
         all_apps.append(app_details)
         get_per_app_data(app_json.get('name'))
     data = {'applicationAtRisk': app_risk, 'findings': findings, 'filesWithFindings': files_findings,
@@ -58,20 +49,13 @@ def get_all_apps_list():
 def get_per_app_data(app_dir):
     app_path = f'{CacheDir.home_dir.value}/{app_dir}/{CacheDir.metadata_file_path.value}'
     app_full_path = get_full_path(app_path)
-    try:
-        with open(app_full_path, "r") as output:
-            app_json = json.load(output)
-            load_id = app_json.get('load_ids')[-1]
 
-    except IOError as err:
-        logger.error(f"No  file found at {app_path}")
+    app_json = read_json_file(app_full_path)
+    load_id = app_json.get('load_ids')[-1]
 
     app_detail_path = f'{CacheDir.home_dir.value}/{app_dir}/{load_id}/{CacheDir.report_data_file_name.value}'
     app_detail_full_path = get_full_path(app_detail_path)
-    try:
-        with open(app_detail_full_path, "r") as output:
-            app_detail_json = json.load(output)
-            return json.dumps(app_detail_json, indent=4)
-    except IOError as err:
-        logger.error(f"No file found at {app_detail_path}")
+
+    app_detail_json = read_json_file(app_detail_full_path)
+    return json.dumps(app_detail_json, indent=4)
 
