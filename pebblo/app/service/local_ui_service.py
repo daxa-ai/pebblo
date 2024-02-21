9aae8cf007e36837f780ba38003fe35a1ae19a53
Kunal Jadhav <47965366+KunalJadhav5@users.noreply.github.com>
2024-02-21 17:25:59 +0530

    Added DataSources Details for Dashboard Tabs (#195)
    * Code changes for dashboard.

--- pebblo/app/service/local_ui_service.py
+++ pebblo/app/service/local_ui_service.py
@@ -2,76 +2,105 @@ import json
 import os
 from pebblo.app.enums.enums import CacheDir
 from pebblo.app.libs.logger import logger
-from pebblo.app.utils.utils import get_full_path
+from pebblo.app.utils.utils import get_full_path, read_json_file
+from pebblo.app.models.models import AppListDetails, AppModel
 
 
-def get_all_apps_list():
-    dir_full_path = get_full_path(CacheDir.home_dir.value)
-    dir_path = os.listdir(dir_full_path)
-    all_apps = []
-    app_risk = 0
-    findings = 0
-    files_findings = 0
-    data_source = 0
-
-    app_details = dict()
-    for app_dir in dir_path:
-        app_path = f'{CacheDir.home_dir.value}/{app_dir}/{CacheDir.metadata_file_path.value}'
-        app_full_path = get_full_path(app_path)
+class AppData:
+    @staticmethod
+    def get_all_apps_details():
+        """Returns all necessary app details required for listing."""
         try:
-            with open(app_full_path, "r") as output:
-                app_json = json.load(output)
+            dir_full_path = get_full_path(CacheDir.home_dir.value)
+            # List all apps in the directory
+            dir_path = os.listdir(dir_full_path)
+
+            # Default values
+            data_source_list = []
+            all_apps = []
+            apps_at_risk = 0
+            findings = 0
+            files_findings = 0
+            data_source = 0
+
+            # Iterating through each app in the directory
+            for app_dir in dir_path:
+                # Path to metadata.json
+                app_path = f'{CacheDir.home_dir.value}/{app_dir}/{CacheDir.metadata_file_path.value}'
+                logger.debug(f'metadata.json path {app_path}')
                 app_details = dict()
-                app_details['name'] = app_json.get('name')
-                app_details['loadId'] = app_json.get('load_ids')[-1]
-                
-        except IOError as err:
-            logger.error(f"No  file found at {app_path}")
+                app_json = read_json_file(app_path)
+                # Condition for handling loadId
+                if app_json and app_json.get('load_ids') is not None and len(app_json.get('load_ids')) > 0:
+                    # Fetching latest loadId
+                    latest_load_id = app_json.get("load_ids")[-1]
+                    # Path to report.json
+                    app_detail_path = f'{CacheDir.home_dir.value}/{app_dir}/{latest_load_id}/{CacheDir.report_data_file_name.value}'
+                    logger.debug(f'report.json path {app_detail_path}')
+                    app_detail_json = read_json_file(app_detail_path)
+                    if app_detail_json:
+                        report_summary = app_detail_json.get('reportSummary')
+                        app_details = AppListDetails(
+                            name=app_json.get('name'),
+                            topics=report_summary.get('findingsTopics', 0),
+                            entities=report_summary.get('findingsEntities', 0),
+                            owner=report_summary.get('owner'),
+                            loadId=latest_load_id
+                        )
+                        data_source_list.append(app_detail_json.get('dataSources'))
+                        findings += report_summary.get('findings', 0)
+                        files_findings += report_summary.get('filesWithFindings', 0)
+                        data_source += report_summary.get('dataSources', 0)
+                        if report_summary.get('findings', 0) > 0:
+                            apps_at_risk += 1
 
-        app_detail_path = f'{CacheDir.home_dir.value}/{app_dir}/{app_json.get("load_ids")[-1]}/{CacheDir.report_data_file_name.value}'
-        print()
-        app_detail_full_path = get_full_path(app_detail_path)
+                        all_apps.append(app_details.dict())
+                else:
+                    logger.debug('Error: Unable to fetch loadId details')
+                    logger.debug(f'App Json : {app_json}')
+
+            # Validation
+            data = AppModel(
+                applicationsAtRiskCount=apps_at_risk,
+                findingsCount=findings,
+                documentsWithFindingsCount=files_findings,
+                dataSourceCount=data_source,
+                appList=all_apps,
+                dataSources=data_source_list
+            )
+
+            return json.dumps(data.dict(), indent=4)
+
+        except Exception as ex:
+            logger.error(f"Error in process_request. Error:{ex}")
+
+    @staticmethod
+    def get_app_details(app_dir):
         try:
-            with open(app_detail_full_path, "r") as output:
-                app_detail_json = json.load(output)
-                report_summary = app_detail_json.get('reportSummary')
-                app_details['topics'] = report_summary.get('findingsTopics', 0)
-                app_details['entities'] = report_summary.get('findingsEntities', 0)
-                app_details['owner'] = report_summary.get('owner')
-                findings += report_summary.get('findings', 0)
-                files_findings += report_summary.get('filesWithFindings', 0)
-                data_source += report_summary.get('dataSources', 0)
-                if report_summary.get('findings', 0) > 0:
-                    app_risk += 1
-                app_details['loadId'] = app_json.get('load_ids')[-1]
+            # Path to metadata.json
+            app_path = f'{CacheDir.home_dir.value}/{app_dir}/{CacheDir.metadata_file_path.value}'
+            logger.debug(f'metadata.json path {app_path}')
+            # Reading metadata.json
+            app_json = read_json_file(app_path)
+            # Condition for handling loadId
+            if app_json and app_json.get('load_ids') is not None and len(app_json.get('load_ids')) > 0:
+                # Fetching latest loadId
+                latest_load_id = app_json.get('load_ids')[-1]
 
-        except IOError as err:
-            logger.error(f"No  file found at {app_detail_path}")
-        all_apps.append(app_details)
-        get_per_app_data(app_json.get('name'))
-    data = {'applicationAtRisk': app_risk, 'findings': findings, 'filesWithFindings': files_findings,
-            'dataSource': data_source, 'appList': all_apps}
+                # Path to report.json
+                app_detail_path = f'{CacheDir.home_dir.value}/{app_dir}/{latest_load_id}/{CacheDir.report_data_file_name.value}'
+                logger.debug(f'metadata.json path {app_detail_path}')
 
-    return json.dumps(data, indent=4)
+                # Reading report.json
+                app_detail_json = read_json_file(app_detail_path)
+                if app_detail_json:
+                    return json.dumps(app_detail_json, indent=4)
+                else:
+                    return json.dumps({})
 
+            else:
+                logger.debug('Error: Unable to fetch loadId details')
+                logger.debug(f'App Json : {app_json}')
 
-def get_per_app_data(app_dir):
-    app_path = f'{CacheDir.home_dir.value}/{app_dir}/{CacheDir.metadata_file_path.value}'
-    app_full_path = get_full_path(app_path)
-    try:
-        with open(app_full_path, "r") as output:
-            app_json = json.load(output)
-            load_id = app_json.get('load_ids')[-1]
-
-    except IOError as err:
-        logger.error(f"No  file found at {app_path}")
-
-    app_detail_path = f'{CacheDir.home_dir.value}/{app_dir}/{load_id}/{CacheDir.report_data_file_name.value}'
-    app_detail_full_path = get_full_path(app_detail_path)
-    try:
-        with open(app_detail_full_path, "r") as output:
-            app_detail_json = json.load(output)
-            return json.dumps(app_detail_json, indent=4)
-    except IOError as err:
-        logger.error(f"No file found at {app_detail_path}")
-
+        except Exception as ex:
+            logger.error(f"Error in process_request. Error:{ex}")
