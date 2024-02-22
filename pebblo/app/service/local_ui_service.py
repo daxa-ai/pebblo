9277bc2f38228dfc9b22b78e7120cfd69dbfb0f3
Kunal Jadhav <47965366+KunalJadhav5@users.noreply.github.com>
2024-02-22 17:06:55 +0530

    Updated finding summary list

--- pebblo/app/service/local_ui_service.py
+++ pebblo/app/service/local_ui_service.py
@@ -93,7 +93,7 @@ class AppData:
             dataSource=data_source_list
         )
 
-        print(f'----Data {data} ----')
+        print(f'----Data {data.dict()} ----')
         return json.dumps(data.dict(), indent=4)
 
         #except Exception as ex:
