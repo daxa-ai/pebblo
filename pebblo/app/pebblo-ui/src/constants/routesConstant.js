28509301dd7a7e99adee4f2eba03ab7c670d992f
Nitin Kumar <nitinkumardeshmukh19@gmail.com>
2024-02-26 20:50:56 +0530

    fix: local ui router end point fix

--- pebblo/app/pebblo-ui/src/constants/routesConstant.js
+++ pebblo/app/pebblo-ui/src/constants/routesConstant.js
@@ -2,5 +2,5 @@ export const PROXY = document.scripts[0].getAttribute("proxy");
 const PREFIX = "/pebblo";
 
 export const DASHBOARD_ROUTE = PREFIX + "/";
-export const APP_DETAILS_ROUTE = PREFIX + "/appDetails/";
-export const GET_REPORT = `${PROXY}${PREFIX}/getReport/`;
+export const APP_DETAILS_ROUTE = PREFIX + "/app/";
+export const GET_REPORT = `${PROXY}${PREFIX}/report/`;
