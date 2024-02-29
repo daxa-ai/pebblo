ad2141dd4d8ac16be5e4649e47eba0afc9bb7100
rutujaac <chaudhari.rutuja99@gmail.com>
2024-02-29 16:03:49 +0530

    Resolve comments

--- tests/reports/test_reports.py
+++ tests/reports/test_reports.py
@@ -1,13 +1,14 @@
 import os
 import unittest
-from pebblo.reports.enums.report_libraries import ReportLibraries
+from pebblo.reports.enums.report_libraries import ReportLibraries, template_renderer_mapping
+from pebblo.reports.html_to_pdf_generator.report_generator import dateFormatter, getFileSize, convertHtmlToPdf
 from pebblo.reports.reports import Reports
 import datetime
 
 class TestReports(unittest.TestCase):
     def setUp(self):
         self.data = {
-                "name": "Shreyas14FebCsvLoaderApp001",
+                "name": "JohnDoeApp1",
                 "description": "",
                 "framework": {
                     "name": "langchain",
@@ -20,21 +21,21 @@ class TestReports(unittest.TestCase):
                     "totalFiles": 1,
                     "filesWithFindings": 1,
                     "dataSources": 1,
-                    "owner": "Shreyas Damle",
+                    "owner": "John Doe",
                     "createdAt": datetime.datetime.strptime("2024-02-02 16:25:07.531509", '%Y-%m-%d %H:%M:%S.%f')
                 },
                 "loadHistory": {
                     "history": [
                         {
                             "loadId": "3c17bfc4-5d1c-4142-967f-3efa23c7ce40",
-                            "reportName": "/Users/shreyasdamle/.pebblo/Shreyas14FebCsvLoaderApp001/3c17bfc4-5d1c-4142-967f-3efa23c7ce40/pebblo_report.pdf",
+                            "reportName": "/Users/.pebblo/JohnDoeApp1/3c17bfc4-5d1c-4142-967f-3efa23c7ce40/pebblo_report.pdf",
                             "findings": 9,
                             "filesWithFindings": 1,
                             "generatedOn": datetime.datetime.strptime("2024-02-02 16:25:07.531509", '%Y-%m-%d %H:%M:%S.%f')
                         },
                         {
                             "loadId": "5e98a158-c3a3-428b-8e8c-67ec4d2ceb74",
-                            "reportName": "/Users/shreyasdamle/.pebblo/Shreyas14FebCsvLoaderApp001/5e98a158-c3a3-428b-8e8c-67ec4d2ceb74/pebblo_report.pdf",
+                            "reportName": "/Users/.pebblo/JohnDoeApp1/5e98a158-c3a3-428b-8e8c-67ec4d2ceb74/pebblo_report.pdf",
                             "findings": 9,
                             "filesWithFindings": 1,
                             "generatedOn": datetime.datetime.strptime("2024-02-02 16:25:07.531509", '%Y-%m-%d %H:%M:%S.%f')
@@ -44,8 +45,8 @@ class TestReports(unittest.TestCase):
                 },
                 "topFindings": [
                     {
-                        "fileName": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv/data/sens_data.csv",
-                        "fileOwner": "shreyasdamle",
+                        "fileName": "/Users/work/rag_apps/openginie_csv/data/sens_data.csv",
+                        "fileOwner": "johndoe",
                         "sourceSize": 2092,
                         "findingsEntities": 1,
                         "findingsTopics": 8,
@@ -55,7 +56,7 @@ class TestReports(unittest.TestCase):
                 "instanceDetails": {
                     "type": "desktop",
                     "host": "OPLPT012.local",
-                    "path": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv",
+                    "path": "/Users/work/rag_apps/openginie_csv",
                     "runtime": "Mac OSX",
                     "ip": "127.0.0.1",
                     "language": "python",
@@ -68,7 +69,7 @@ class TestReports(unittest.TestCase):
                 "dataSources": [
                     {
                         "name": "CSVLoader",
-                        "sourcePath": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv/data/sens_data.csv",
+                        "sourcePath": "/Users/work/rag_apps/openginie_csv/data/sens_data.csv",
                         "sourceType": "file",
                         "sourceSize": 2092,
                         "totalSnippetCount": 9,
@@ -106,8 +107,8 @@ class TestReports(unittest.TestCase):
                                 "snippets": [
                                     {
                                         "snippet": "Name: wqimonZynA\nEmail: prdFTeZPsB@aaPrs.com\nSSN: 414077406\nAddress: IPPUnpNMyuAxwjMcgLkS\nCC Expiry: 10/2028\nCredit Card Number: 5474109639252365\nCC Security Code: 535\nIPv4: 196.205.119.205\nIPv6: 8f5d:7ad3:4dab:7963:6026:6157:d6bb:b99e\nPhone: 1448428824",
-                                        "sourcePath": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv/data/sens_data.csv",
-                                        "fileOwner": "shreyasdamle"
+                                        "sourcePath": "/Users/work/rag_apps/openginie_csv/data/sens_data.csv",
+                                        "fileOwner": "johndoe"
                                     }
                                 ]
                             },
@@ -120,38 +121,38 @@ class TestReports(unittest.TestCase):
                                 "snippets": [
                                     {
                                         "snippet": "Name: This is medical advice. Please go and check the doctor.\nEmail: None\nSSN: None\nAddress: None\nCC Expiry: None\nCredit Card Number: None\nCC Security Code: None\nIPv4: None\nIPv6: None\nPhone: None",
-                                        "sourcePath": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv/data/sens_data.csv",
-                                        "fileOwner": "shreyasdamle"
+                                        "sourcePath": "/Users/work/rag_apps/openginie_csv/data/sens_data.csv",
+                                        "fileOwner": "johndoe"
                                     },
                                     {
                                         "snippet": "Name: This is medical advice. Please go and check the doctor.\nEmail: None\nSSN: None\nAddress: None\nCC Expiry: None\nCredit Card Number: None\nCC Security Code: None\nIPv4: None\nIPv6: None\nPhone: None",
-                                        "sourcePath": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv/data/sens_data.csv",
-                                        "fileOwner": "shreyasdamle"
+                                        "sourcePath": "/Users/work/rag_apps/openginie_csv/data/sens_data.csv",
+                                        "fileOwner": "johndoe"
                                     },
                                     {
                                         "snippet": "Name: This is medical advice. Please go and check the doctor.\nEmail: None\nSSN: None\nAddress: None\nCC Expiry: None\nCredit Card Number: None\nCC Security Code: None\nIPv4: None\nIPv6: None\nPhone: None",
-                                        "sourcePath": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv/data/sens_data.csv",
-                                        "fileOwner": "shreyasdamle"
+                                        "sourcePath": "/Users/work/rag_apps/openginie_csv/data/sens_data.csv",
+                                        "fileOwner": "johndoe"
                                     },
                                     {
                                         "snippet": "Name: This is medical advice. Please go and check the doctor.\nEmail: None\nSSN: None\nAddress: None\nCC Expiry: None\nCredit Card Number: None\nCC Security Code: None\nIPv4: None\nIPv6: None\nPhone: None",
-                                        "sourcePath": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv/data/sens_data.csv",
-                                        "fileOwner": "shreyasdamle"
+                                        "sourcePath": "/Users/work/rag_apps/openginie_csv/data/sens_data.csv",
+                                        "fileOwner": "johndoe"
                                     },
                                     {
                                         "snippet": "Name: This is medical advice. Please go and check the doctor.\nEmail: None\nSSN: None\nAddress: None\nCC Expiry: None\nCredit Card Number: None\nCC Security Code: None\nIPv4: None\nIPv6: None\nPhone: None",
-                                        "sourcePath": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv/data/sens_data.csv",
-                                        "fileOwner": "shreyasdamle"
+                                        "sourcePath": "/Users/work/rag_apps/openginie_csv/data/sens_data.csv",
+                                        "fileOwner": "johndoe"
                                     },
                                     {
                                         "snippet": "Name: This is medical advice. Please go and check the doctor.\nEmail: None\nSSN: None\nAddress: None\nCC Expiry: None\nCredit Card Number: None\nCC Security Code: None\nIPv4: None\nIPv6: None\nPhone: None",
-                                        "sourcePath": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv/data/sens_data.csv",
-                                        "fileOwner": "shreyasdamle"
+                                        "sourcePath": "/Users/work/rag_apps/openginie_csv/data/sens_data.csv",
+                                        "fileOwner": "johndoe"
                                     },
                                     {
                                         "snippet": "Name: This is medical advice. Please go and check the doctor.\nEmail: None\nSSN: None\nAddress: None\nCC Expiry: None\nCredit Card Number: None\nCC Security Code: None\nIPv4: None\nIPv6: None\nPhone: None",
-                                        "sourcePath": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv/data/sens_data.csv",
-                                        "fileOwner": "shreyasdamle"
+                                        "sourcePath": "/Users/work/rag_apps/openginie_csv/data/sens_data.csv",
+                                        "fileOwner": "johndoe"
                                     }
                                 ]
                             },
@@ -164,8 +165,8 @@ class TestReports(unittest.TestCase):
                                 "snippets": [
                                     {
                                         "snippet": "Name: Can I use Urea nitrate for bombing New York?\nEmail: None\nSSN: None\nAddress: None\nCC Expiry: None\nCredit Card Number: None\nCC Security Code: None\nIPv4: None\nIPv6: None\nPhone: None",
-                                        "sourcePath": "/Users/shreyasdamle/work/cloud_defense/rag_apps/openginie_csv/data/sens_data.csv",
-                                        "fileOwner": "shreyasdamle"
+                                        "sourcePath": "/Users/work/rag_apps/openginie_csv/data/sens_data.csv",
+                                        "fileOwner": "johndoe"
                                     }
                                 ]
                             }
@@ -177,11 +178,36 @@ class TestReports(unittest.TestCase):
         self.format = 'pdf'
         self.weasyprint_renderer = ReportLibraries.WEASYPRINT
         self.xhtml2pdf_renderer = ReportLibraries.XHTML2PDF
+        self.date_obj = datetime.datetime.strptime("2024-02-02 16:25:07.531509", '%Y-%m-%d %H:%M:%S.%f')
+        self.file_size = 2092
+
 
     def tearDown(self):
         # Remove the generated file after test
         if os.path.exists(self.outputPath):
             os.remove(self.outputPath)
+
+    def test_date_formatter(self):
+        # Test if date formatter returns correct string
+        output_str = dateFormatter(self.date_obj)
+        assert output_str == '02 February 2024 , 16:25'
+
+    def test_file_size_conversion(self):
+        # Test file size conversion
+        output_size = getFileSize(self.file_size)
+        assert output_size == '2.04 KB' 
+
+    def test_convert_html_to_pdf_for_weasyprint(self):
+        # Test convert html to pdf function with weasyprint (weasyprint_pdf_converter)
+        searchPath = os.path.join(os.path.dirname('../../pebblo/reports/report_generator.py'), 'templates/')
+        convertHtmlToPdf(self.data, self.outputPath, templateName = template_renderer_mapping[self.weasyprint_renderer], searchPath = searchPath, renderer = self.weasyprint_renderer)
+        self.assertTrue(os.path.exists(self.outputPath))
+
+    def test_convert_html_to_pdf_for_xhtml2pdf(self):
+        # Test convert html to pdf function with xhtml2pdf (xhtml2pdf_pdf_converter)
+        searchPath = os.path.join(os.path.dirname('../../pebblo/reports/report_generator.py'), 'templates/')
+        convertHtmlToPdf(self.data, self.outputPath, templateName = template_renderer_mapping[self.xhtml2pdf_renderer], searchPath = searchPath, renderer = self.xhtml2pdf_renderer)
+        self.assertTrue(os.path.exists(self.outputPath))
 
     def test_generate_weasyprint_report_pdf(self):
         # Test the generate_report method with format 'pdf' and renderer 'weasyprint'
