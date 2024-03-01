45eae50725e1a0ab7505847575a04bfcfa5fff85
rutujaac <chaudhari.rutuja99@gmail.com>
2024-03-01 16:26:15 +0530

    Updated Report Module Formatting using ruff

--- pebblo/reports/html_to_pdf_generator/report_generator.py
+++ pebblo/reports/html_to_pdf_generator/report_generator.py
@@ -3,20 +3,22 @@ import datetime
 from decimal import Decimal
 from pebblo.reports.enums.report_libraries import library_function_mapping
 
+
 # Converts date string to object and returns formatted string for date (D M Y, H:M)
 def dateFormatter(date_obj):
-    return date_obj.strftime('%d %B %Y , %H:%M')
+    return date_obj.strftime("%d %B %Y , %H:%M")
+
 
 # Returns file size in KB, MB, GB as applicable
 def getFileSize(size):
     power = 2**10
     n = 0
-    power_labels = {0 : 'Bytes', 1: 'KB', 2: 'MB', 3: 'GB', 4: 'TB'}
+    power_labels = {0: "Bytes", 1: "KB", 2: "MB", 3: "GB", 4: "TB"}
     while size > power:
         size /= power
         n += 1
     sizeNum = Decimal(str(size))
-    sizeStr = str(round(sizeNum, 2)) + " " + power_labels.get(n, '')
+    sizeStr = str(round(sizeNum, 2)) + " " + power_labels.get(n, "")
     return sizeStr
 
 
@@ -25,6 +27,14 @@ def convertHtmlToPdf(data, outputPath, templateName, searchPath, renderer):
     templateLoader = jinja2.FileSystemLoader(searchpath=searchPath)
     templateEnv = jinja2.Environment(loader=templateLoader)
     template = templateEnv.get_template(templateName)
-    sourceHtml = template.render(data=data, date=datetime.datetime.now(), datastores=data["dataSources"][0], findingDetails=data["dataSources"][0]["findingsDetails"], loadHistoryItemsToDisplay=data["loadHistory"]["history"] , dateFormatter=dateFormatter, getFileSize=getFileSize)
+    sourceHtml = template.render(
+        data=data,
+        date=datetime.datetime.now(),
+        datastores=data["dataSources"][0],
+        findingDetails=data["dataSources"][0]["findingsDetails"],
+        loadHistoryItemsToDisplay=data["loadHistory"]["history"],
+        dateFormatter=dateFormatter,
+        getFileSize=getFileSize,
+    )
     pdfConverter = library_function_mapping[renderer]
     pdfConverter(sourceHtml, outputPath, searchPath)
--- pebblo/reports/reports.py
+++ pebblo/reports/reports.py
@@ -1,20 +1,37 @@
 # Import HTML to PDF generator function
 
 from pebblo.reports.html_to_pdf_generator.report_generator import convertHtmlToPdf
-from pebblo.reports.enums.report_libraries import ReportLibraries, template_renderer_mapping
+from pebblo.reports.enums.report_libraries import (
+    ReportLibraries,
+    template_renderer_mapping,
+)
 from pebblo.reports.libs.logger import logger
 import os
+
 
 class Reports:
     # Generate report - JSON data, output file name, template file name
     @staticmethod
-    def generate_report(data, outputPath='./report.pdf', format = 'pdf', renderer = ReportLibraries.WEASYPRINT):
-        if format == 'pdf':
-            searchPath = os.path.join(os.path.dirname(__file__), 'templates/')
+    def generate_report(
+        data,
+        outputPath="./report.pdf",
+        format="pdf",
+        renderer=ReportLibraries.WEASYPRINT,
+    ):
+        if format == "pdf":
+            searchPath = os.path.join(os.path.dirname(__file__), "templates/")
             try:
                 templateName = template_renderer_mapping[renderer]
-                convertHtmlToPdf(data, outputPath, templateName = templateName, searchPath = searchPath, renderer = renderer)
+                convertHtmlToPdf(
+                    data,
+                    outputPath,
+                    templateName=templateName,
+                    searchPath=searchPath,
+                    renderer=renderer,
+                )
             except Exception as e:
-                logger.error(f"Renderer {renderer} not supported. Please use supported renderers: {ReportLibraries.WEASYPRINT} or {ReportLibraries.XHTML2PDF}, {e}")
+                logger.error(
+                    f"Renderer {renderer} not supported. Please use supported renderers: {ReportLibraries.WEASYPRINT} or {ReportLibraries.XHTML2PDF}, {e}"
+                )
         else:
             logger.error(f"Output file format {format} not supported")
