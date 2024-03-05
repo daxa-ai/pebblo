2741476dbe8568a114e22810c9b036482391b973
Nitin Kumar <nitinkumardeshmukh19@gmail.com>
2024-03-05 10:53:38 +0530

    [Fix] Redirection for local ui implemented (#242)
    * fix: redirection for local ui implemented

---
+++ pebblo/app/api/redirection_api.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from pebblo.app.enums.enums import CacheDir
+
+
+class App:
+    """
+    Controller Class for all the api endpoints for redirection."""
+
+    def __init__(self):
+        self.router = APIRouter()
+
+    @staticmethod
+    def redirect():
+        return f"{CacheDir.proxy.value}/pebblo/"
