9f7698f829d7b84280cee5333b152e7a5dbd14fc
rutujaac <chaudhari.rutuja99@gmail.com>
2024-03-12 17:43:48 +0530

    Fix not-found route

--- pebblo/app/exceptions/exception_handler.py
+++ pebblo/app/exceptions/exception_handler.py
@@ -5,7 +5,7 @@ from fastapi import Request
 
 async def not_found_error(request: Request, exc: HTTPException):
     """Redirects to not-found route when 404 exception occurs"""
-    return RedirectResponse("/pebblo/not-found")
+    return RedirectResponse("/pebblo/not-found/")
 
 
 exception_handlers = {404: not_found_error, 500: not_found_error}
