2e4156c22cbac8d3dad4c99daffb4de92e05da1d
rutujaac <chaudhari.rutuja99@gmail.com>
2024-03-12 14:36:28 +0530

    Page not found - Update page image

--- pebblo/app/pebblo-ui/src/pages/pageNotFound.js
+++ pebblo/app/pebblo-ui/src/pages/pageNotFound.js
@@ -1,13 +1,21 @@
+import { Button } from "../components/index.js";
 import { MEDIA_URL } from "../constants/constant.js";
+import { DASHBOARD_ROUTE } from "../constants/routesConstant.js";
 
 export const PageNotFound = () => {
   return /*html*/ `
-        <div class="inter w-100 h-full flex flex-col gap-4 items-center justify-center">
-            <img src=${`${MEDIA_URL}/static/not-found.png`} alt="page-not-found" height=${150} />
-            <div class="flex flex-col gap-5 items-center">
-                <div class="font-24 medium">This page is not available</div>
-                <div class="font-18">Kindly check the URL you entered</div>
+        <div class="inter w-100 h-full flex flex-col gap-6 items-center justify-center">
+            <img src=${`${MEDIA_URL}/static/not-found.png`} alt="page-not-found" height=${180} />
+            <div class="flex flex-col items-center font-20">
+                <div>Uh-oh! It seems you've stumbled upon a lost page.</div>
+                <div>Let's help you find your way.</div>
             </div>
+            ${Button({
+              variant: "contained",
+              btnText: "Click Here!",
+              href: DASHBOARD_ROUTE,
+              className: "font-16",
+            })}
         </div>
     `;
 };
