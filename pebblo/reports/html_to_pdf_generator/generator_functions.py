e62cd10d91ecd99f01fff13b81c853acbc6cc9c2
Shreyas Damle <damleshreyas+github@gmail.com>
2024-03-14 11:59:54 +0530

    Removed wasyprint(and pango) dependency as we are defaulting to xhtml2pdf library now.

--- pebblo/reports/html_to_pdf_generator/generator_functions.py
+++ pebblo/reports/html_to_pdf_generator/generator_functions.py
@@ -4,13 +4,12 @@ Defines functions to generate PDF from HTML for respective renderers
 
 import os
 
-from weasyprint import CSS, HTML
-from xhtml2pdf import pisa
-
 
 # Creates PDF from template using weasyprint
 def weasyprint_pdf_converter(source_html, output_path, search_path):
     """PDF generator function for weasyprint renderer"""
+    from weasyprint import CSS, HTML
+
     base_url = os.path.dirname(os.path.realpath(__file__))
     html_doc = HTML(string=source_html, base_url=base_url)
     return html_doc.write_pdf(
@@ -21,6 +20,8 @@ def weasyprint_pdf_converter(source_html, output_path, search_path):
 # Creates PDF from template using xhtml2pdf
 def xhtml2pdf_pdf_converter(source_html, output_path, _):
     """PDF generator function for xhtml2pdf renderer"""
+    from xhtml2pdf import pisa
+
     with open(output_path, "w+b") as result_file:
         pisa_status = pisa.CreatePDF(src=source_html, dest=result_file)
         result_file.close()
