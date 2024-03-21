93b67cdf34cb78c996e728423d84f93da5628fa5
Rutuja Chaudhari <36963955+rutujaac@users.noreply.github.com>
2024-03-21 17:49:55 +0530

    Add UT for reports module (#258)
    * Add ut for reports module
    
    * Fix ruff/pylint errors

---
+++ tests/reports/test_report_generator.py
@@ -0,0 +1,75 @@
+"""
+Unit test cases for reports/html_to_pdf_generator/report_generator.py file
+"""
+
+import datetime
+import unittest
+import time
+from unittest.mock import Mock, patch
+from pebblo.reports.html_to_pdf_generator.report_generator import (
+    date_formatter,
+    get_file_size,
+    convert_html_to_pdf,
+)
+
+
+class TestReportGenerator(unittest.TestCase):
+    """Class to hold report_generator UT cases"""
+
+    def setUp(self):
+        """Setup mock data"""
+        self.date_obj = datetime.datetime.strptime(
+            "2024-02-02 16:25:07.531509", "%Y-%m-%d %H:%M:%S.%f"
+        )
+        self.file_size = 2092
+
+    def test_date_formatter(self):
+        """Test if date formatter returns correct string"""
+        output_str = date_formatter(self.date_obj)
+        assert output_str == "02 February 2024 , 16:25" + " " + time.localtime().tm_zone
+
+    def test_file_size_conversion(self):
+        """Test file size conversion"""
+        output_size = get_file_size(self.file_size)
+        assert output_size == "2.04 KB"
+
+    @patch("jinja2.Environment", return_value=Mock(get_template=Mock()))
+    @patch("jinja2.FileSystemLoader")
+    def test_convert_html_to_pdf(self, mock_filesystem_loader, mock_environment):
+        """Test the convert_html_to_pdf function"""
+        # Arrange
+        data = {
+            "dataSources": [{"findingsDetails": "details"}],
+            "loadHistory": {"history": "history"},
+        }
+        output_path = "output_path"
+        template_name = "template_name"
+        search_path = "search_path"
+        renderer = "renderer"
+        mock_pdf_converter = Mock()
+
+        with patch.dict(
+            "pebblo.reports.html_to_pdf_generator.report_generator.library_function_mapping",
+            {renderer: mock_pdf_converter},
+        ):
+            # Act
+            convert_html_to_pdf(data, output_path, template_name, search_path, renderer)
+
+        # Assert
+        mock_filesystem_loader.assert_called_once_with(searchpath=search_path)
+        mock_environment.assert_called_once_with(
+            loader=mock_filesystem_loader.return_value
+        )
+        mock_environment.return_value.get_template.assert_called_once_with(
+            template_name
+        )
+        mock_pdf_converter.assert_called_once()
+        mock_pdf_converter.assert_called_once_with(
+            mock_environment.return_value.get_template.return_value.render.return_value,
+            output_path,
+            search_path,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/reports/test_reports.py
@@ -0,0 +1,76 @@
+"""Unit test cases for reports module"""
+
+from pathlib import Path
+import pytest
+from pebblo.reports.enums.report_libraries import ReportLibraries
+from pebblo.reports.reports import Reports
+
+
+@pytest.fixture
+def convert_html_to_pdf(mocker):
+    """
+    Mock the convert_html_to_pdf function
+    """
+    return mocker.patch(
+        "pebblo.reports.reports.convert_html_to_pdf", return_value=[True, ""]
+    )
+
+
+def test_generate_report(convert_html_to_pdf):
+    """
+    Test the generate_report function
+    """
+    # Get the template path
+    template_path = (
+        str(Path(__file__).parent.parent.parent.absolute())
+        + "/pebblo/reports/templates/"
+    )
+
+    # Call the generate_report method
+    Reports.generate_report(
+        data={},
+        output_path="./report.pdf",
+        format_string="pdf",
+        renderer=ReportLibraries.XHTML2PDF,
+    )
+
+    # Assert that the mock function was called with the correct arguments
+    convert_html_to_pdf.assert_called_once_with(
+        {},
+        "./report.pdf",
+        template_name="xhtml2pdfTemplate.html",
+        search_path=template_path,
+        renderer=ReportLibraries.XHTML2PDF,
+    )
+
+
+def test_generate_report_with_wrong_format_string(convert_html_to_pdf):
+    """
+    Test the report generation function with invalid format string
+    """
+    # Call the generate_report method with wrong format_string
+    Reports.generate_report(
+        data={},
+        output_path="./report.pdf",
+        format_string="wrong_format",
+        renderer=ReportLibraries.XHTML2PDF,
+    )
+
+    # Assert that the mock function was not called
+    convert_html_to_pdf.assert_not_called()
+
+
+def test_generate_report_with_wrong_renderer(convert_html_to_pdf):
+    """
+    Test the report generation function with invalid renderer string
+    """
+    # Call the generate_report method with wrong renderer
+    Reports.generate_report(
+        data={},
+        output_path="./report.pdf",
+        format_string="pdf",
+        renderer="wrong_renderer",
+    )
+
+    # Assert that the mock function was not called
+    convert_html_to_pdf.assert_not_called()
