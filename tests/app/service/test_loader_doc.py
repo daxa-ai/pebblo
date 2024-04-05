14fa3c13aaa72f8d75e71e37888ff5a82169a532
Shreyas Damle <damleshreyas+github@gmail.com>
2024-04-05 19:55:24 +0530

    Ruff fixes.

--- tests/app/service/test_loader_doc.py
+++ tests/app/service/test_loader_doc.py
@@ -16,7 +16,7 @@ data = {
             "last_modified": datetime.datetime.now(),
             "file_owner": "FileOwner",
             "source_path_size": 1000,
-            "authorizedIdentities": []
+            "authorizedIdentities": [],
         }
     ],
     "plugin_version": "0.1.0",
@@ -110,7 +110,7 @@ def test_get_doc_report_metadata(loader_helper):
         "fileOwner": "fileOwner",
         "sourceSize": 1000,
         "sourcePath": "/home/ubuntu/sens_data.csv",
-        "authorizedIdentities": []
+        "authorizedIdentities": [],
     }
 
     output = loader_helper._get_doc_report_metadata(doc, raw_data)
@@ -122,7 +122,7 @@ def test_get_doc_report_metadata(loader_helper):
         "data_source_snippets": [],
         "loader_source_snippets": {
             "/home/ubuntu/sens_data.csv": {
-                'authorized_identities': [],
+                "authorized_identities": [],
                 "findings_entities": 2,
                 "findings_topics": 1,
                 "findings": 3,
@@ -142,7 +142,7 @@ def test_get_doc_report_metadata(loader_helper):
                 "unique_snippets": {"/home/ubuntu/sens_data.csv"},
                 "snippets": [
                     {
-                        'authorizedIdentities': [],
+                        "authorizedIdentities": [],
                         "snippet": "sample doc",
                         "sourcePath": "/home/ubuntu/sens_data.csv",
                         "fileOwner": "fileOwner",
@@ -158,7 +158,7 @@ def test_get_doc_report_metadata(loader_helper):
                 "unique_snippets": {"/home/ubuntu/sens_data.csv"},
                 "snippets": [
                     {
-                        'authorizedIdentities': [],
+                        "authorizedIdentities": [],
                         "snippet": "sample doc",
                         "sourcePath": "/home/ubuntu/sens_data.csv",
                         "fileOwner": "fileOwner",
@@ -174,7 +174,7 @@ def test_get_doc_report_metadata(loader_helper):
                 "unique_snippets": {"/home/ubuntu/sens_data.csv"},
                 "snippets": [
                     {
-                        'authorizedIdentities': [],
+                        "authorizedIdentities": [],
                         "snippet": "sample doc",
                         "sourcePath": "/home/ubuntu/sens_data.csv",
                         "fileOwner": "fileOwner",
@@ -219,7 +219,7 @@ def test_get_finding_details(loader_helper):
             "unique_snippets": {"/home/ubuntu/sens_data.csv"},
             "snippets": [
                 {
-                    'authorizedIdentities': [],
+                    "authorizedIdentities": [],
                     "snippet": "Sample Doc",
                     "sourcePath": "/home/ubuntu/sens_data.csv",
                     "fileOwner": "fileOwner",
@@ -235,7 +235,7 @@ def test_get_finding_details(loader_helper):
             "unique_snippets": {"/home/ubuntu/sens_data.csv"},
             "snippets": [
                 {
-                    'authorizedIdentities': [],
+                    "authorizedIdentities": [],
                     "snippet": "Sample Doc",
                     "sourcePath": "/home/ubuntu/sens_data.csv",
                     "fileOwner": "fileOwner",
@@ -251,7 +251,7 @@ def test_get_finding_details(loader_helper):
             "unique_snippets": {"/home/ubuntu/sens_data.csv"},
             "snippets": [
                 {
-                    'authorizedIdentities': [],
+                    "authorizedIdentities": [],
                     "snippet": "Sample Doc",
                     "sourcePath": "/home/ubuntu/sens_data.csv",
                     "fileOwner": "fileOwner",
@@ -322,7 +322,7 @@ def test_update_app_details(loader_helper):
                         "findings_entities": 2,
                         "findings_topics": 1,
                         "findings": 3,
-                        "authorized_identities": []
+                        "authorized_identities": [],
                     }
                 ],
             }
@@ -379,7 +379,7 @@ def test_get_top_n_findings(loader_helper):
             "findingsEntities": 2,
             "findingsTopics": 1,
             "findings": 3,
-            "authorizedIdentities": []
+            "authorizedIdentities": [],
         }
     ]
 
