27e1c2bbf153011488e2f9e3486ece3c430d83c3
Dristy Srivastava <58721149+dristysrivastava@users.noreply.github.com>
2024-04-22 12:21:17 +0530

    Changes in /prompt API (#333)
    * Changes in /prompt API

--- pebblo/app/service/prompt_service.py
+++ pebblo/app/service/prompt_service.py
@@ -32,20 +32,22 @@ class Prompt:
         """
         logger.debug("Retrieving retrieval context details from input data")
         # Fetching retrieval context details
-        retrieval_context = {}
+        retrieval_context_output = []
         context_data = self.data.get("context")
-        fields_validator(context_data, ["retrieved_from", "doc", "vector_db"])
         if context_data:
-            retrieval_context = RetrievalContext(
-                retrieved_from=context_data.get("retrieved_from"),
-                doc=context_data.get("doc"),
-                vector_db=context_data.get("vector_db"),
-            )
+            for data in context_data:
+                fields_validator(data, ["retrieved_from", "doc", "vector_db"])
+                retrieval_context = RetrievalContext(
+                    retrieved_from=data.get("retrieved_from"),
+                    doc=data.get("doc"),
+                    vector_db=data.get("vector_db"),
+                )
+                retrieval_context_output.append(retrieval_context.dict())
 
         logger.debug(
-            f"AI_APPS [{self.application_name}]: Retrieval Context Details: {retrieval_context.dict()}"
+            f"AI_APPS [{self.application_name}]: Retrieval Context Details: {retrieval_context_output}"
         )
-        return retrieval_context
+        return retrieval_context_output
 
     def _fetch_context_data(self, param):
         """
@@ -56,6 +58,7 @@ class Prompt:
         data = {}
         context_data = self.data.get(param)
         if context_data:
+            fields_validator(context_data, ["data"])
             data = AiDataModel(
                 data=context_data.get("data"),
                 entityCount=context_data.get("entityCount")
@@ -82,7 +85,7 @@ class Prompt:
         Create an RetrievalData Model and return the corresponding model object
         """
         logger.debug("Creating RetrievalData model")
-
+        fields_validator(self.data, ["prompt_time", "user"])
         retrieval_data_model = RetrievalData(
             context=retrieval_context_data,
             prompt=prompt_data,
