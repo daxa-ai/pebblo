99a800e2710b7cb94e12f9e546247d681b798ba1
rutujaac <chaudhari.rutuja99@gmail.com>
2024-04-26 17:56:23 +0530

    Removed unused vars

--- pebblo/app/pebblo-ui/src/constants/routesConstant.js
+++ pebblo/app/pebblo-ui/src/constants/routesConstant.js
@@ -1,6 +1,4 @@
 const PREFIX = "/pebblo";
-const SCRIPT_ELEMENT = document.getElementById("main_script");
-export const PROXY = SCRIPT_ELEMENT.dataset["proxy"];
 export const DASHBOARD_ROUTE = PREFIX + "/";
 export const APP_DETAILS_ROUTE = PREFIX + "/app/";
 export const GET_REPORT = PREFIX + "/report/";
