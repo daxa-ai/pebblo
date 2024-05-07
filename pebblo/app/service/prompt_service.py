1e79dc60599c38102b45cff4a0f1fb1b605240e2
Dristy Srivastava <58721149+dristysrivastava@users.noreply.github.com>
2024-05-07 11:27:33 +0530

    Adding lint fixes (#380)
    * Adding lint fix

--- pebblo/app/service/prompt_service.py
+++ pebblo/app/service/prompt_service.py
@@ -59,23 +59,25 @@ class Prompt:
         context_data = self.data.get(param)
         if context_data:
             fields_validator(context_data, ["data"])
-            data = AiDataModel(
-                data=context_data.get("data"),
-                entityCount=context_data.get("entityCount")
-                if context_data.get("entityCount")
-                else 0,
-                entities=context_data.get("entities")
-                if context_data.get("entities")
-                else {},
-                topicCount=context_data.get("topicCount")
-                if context_data.get("topicCount")
-                else 0,
-                topics=context_data.get("topics") if context_data.get("topics") else {},
-            )
+            data = (
+                AiDataModel(
+                    data=context_data.get("data"),
+                    entityCount=context_data.get("entityCount")
+                    if context_data.get("entityCount")
+                    else 0,
+                    entities=context_data.get("entities")
+                    if context_data.get("entities")
+                    else {},
+                    topicCount=context_data.get("topicCount")
+                    if context_data.get("topicCount")
+                    else 0,
+                    topics=context_data.get("topics")
+                    if context_data.get("topics")
+                    else {},
+                )
+            ).dict()
 
-        logger.debug(
-            f"AI_APPS [{self.application_name}]: {param} Details: {data.dict()}"
-        )
+        logger.debug(f"AI_APPS [{self.application_name}]: {param} Details: {data}")
         return data
 
     def _create_retrieval_data(
@@ -90,9 +92,9 @@ class Prompt:
             context=retrieval_context_data,
             prompt=prompt_data,
             response=response_data,
-            prompt_time=self.data.get("prompt_time"),
-            user=self.data.get("user"),
-            linked_groups=self.data.get("user_identities"),
+            prompt_time=self.data["prompt_time"],
+            user=self.data["user"],
+            linked_groups=self.data.get("user_identities", []),
         )
 
         logger.debug(
