6e96d77a7f93a65cd8568551b4a1e7b4d604614b
Shreyas Damle <57351398+shreyas-damle@users.noreply.github.com>
2024-07-22 17:45:49 +0530

    Add Delete App (#416)
    * Delete Application API Integrated
    
    * Added Unit test cases for delete app
    
    * Ruff fixes and UT fixes.
    
    * Incorporated review comments.

--- tests/app/test_utils.py
+++ tests/app/test_utils.py
@@ -1,6 +1,12 @@
+import tempfile
+
 import toml
 
-from pebblo.app.utils.utils import get_pebblo_server_version
+from pebblo.app.utils.utils import (
+    delete_directory,
+    get_full_path,
+    get_pebblo_server_version,
+)
 
 
 def test_get_pebblo_server_version():
@@ -10,3 +16,25 @@ def test_get_pebblo_server_version():
 
     pebblo_server_version = get_pebblo_server_version()
     assert pebblo_version == pebblo_server_version
+
+
+def test_delete_directory():
+    # make sample directory
+    dir_path = tempfile.mkdtemp()
+    full_path = get_full_path(dir_path)
+    response = delete_directory(full_path, dir_path)
+    assert response == {
+        "message": f"Application {dir_path} has been deleted.",
+        "status_code": 200,
+    }
+
+
+def test_delete_directory_dir_not_exist():
+    app_name = "test_dir"
+    dir_path = ".pebblo/test_dir"
+    full_path = get_full_path(dir_path)
+    response = delete_directory(full_path, app_name)
+    assert response == {
+        "message": f"Application {app_name} does not exist.",
+        "status_code": 404,
+    }
