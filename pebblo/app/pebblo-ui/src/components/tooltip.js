bf5132af9e87d019422b5928c9f6b00c4023a2c1
rutujaac <chaudhari.rutuja99@gmail.com>
2024-08-21 15:39:54 +0530

    add confidence score at snippet level

--- pebblo/app/pebblo-ui/src/components/tooltip.js
+++ pebblo/app/pebblo-ui/src/components/tooltip.js
@@ -5,10 +5,12 @@
 // }
 
 export function Tooltip(props) {
-  const { children, title, variant = "top", inline } = props;
+  const { children, title, variant = "top", inline, width } = props;
   return /*html*/ `
   <span class="tooltip ${inline ? "tooltip-inline" : ""}">
   <span class="tooltip-content">${children}</span>
-  <span class="tooltip-wrapper tooltip-wrapper-${variant}"><span class="tooltip-title-${variant}">${title}</span></span></span>
+  <span class="tooltip-wrapper tooltip-wrapper-${variant} ${
+    width || ""
+  }"><span class="tooltip-title-${variant}">${title}</span></span></span>
    `;
 }
