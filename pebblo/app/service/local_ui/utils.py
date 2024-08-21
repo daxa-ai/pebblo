485fbb476688bbd43a197a36a58217778cede191
dristy.cd <dristy@clouddefense.io>
2024-08-21 15:35:41 +0530

    lint fix

--- pebblo/app/service/local_ui/utils.py
+++ pebblo/app/service/local_ui/utils.py
@@ -1,11 +1,10 @@
 from pebblo.app.enums.enums import ApplicationTypes
-from pebblo.app.models.sqltables import AiDataLoaderTable, AiAppTable
+from pebblo.app.models.sqltables import AiAppTable, AiDataLoaderTable
 
 
 def check_if_loader_app(db, app_name):
     _, ai_loader_app_obj = db.query(
-        table_obj=AiDataLoaderTable,
-        filter_query={"name": app_name}
+        table_obj=AiDataLoaderTable, filter_query={"name": app_name}
     )
     if ai_loader_app_obj and len(ai_loader_app_obj) > 0:
         return True
@@ -13,10 +12,7 @@ def check_if_loader_app(db, app_name):
 
 
 def check_if_retriever_app(db, app_name):
-    _, ai_app_obj = db.query(
-        table_obj=AiAppTable,
-        filter_query={"name": app_name}
-    )
+    _, ai_app_obj = db.query(table_obj=AiAppTable, filter_query={"name": app_name})
     if ai_app_obj and len(ai_app_obj) > 0:
         return True
     return False
