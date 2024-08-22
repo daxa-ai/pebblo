01a78f6d4699442b605621af6a5ccccf87ceb0aa
Shreyas Damle <damleshreyas+github@gmail.com>
2024-08-22 10:00:43 +0530

    Ruff fixes.

--- pebblo/app/service/local_ui/loader_apps.py
+++ pebblo/app/service/local_ui/loader_apps.py
@@ -88,9 +88,7 @@ class LoaderApp:
                         "findingsType": "entities",
                         "snippetCount": len(entity_data["snippetIds"]),
                         "fileCount": len(app_data["documents"]),
-                        "snippets": self._get_snippet_details(
-                            entity_data["snippetIds"]
-                        ),
+                        "snippets": self._get_snippet_details(entity_data["snippetIds"]),
                     }
                     total_snippet_count += findings["snippetCount"]
                     shallow_copy = findings.copy()
