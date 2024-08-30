61f5cf72d6e51282790ab125103481d07218438e
Nishan Jain <gr8nishan@gmail.com>
2024-08-30 16:20:21 +0530

    added \b after testing

--- pebblo/entity_classifier/utils/regex_pattern.py
+++ pebblo/entity_classifier/utils/regex_pattern.py
@@ -5,13 +5,13 @@ These are all enums related to Regex Patterns.
 """
 
 regex_secrets_patterns = {
-    "github-token": r"""((?:ghp|gho|ghu|ghs|ghr)_[a-zA-Z0-9]{36,255})""",
-    "github-finergrained-token": r"""((?:github_pat)_[a-zA-Z0-9_]{36,255})""",
+    "github-token": r"""\b((?:ghp|gho|ghu|ghs|ghr)_[a-zA-Z0-9]{36,255})\b""",
+    "github-finergrained-token": r"""\b((?:github_pat)_[a-zA-Z0-9_]{36,255})\b""",
     "slack-token": r"""(xoxb|xoxp|xapp|xoxa|xoxr|xoxo|xoxs|xoxe)\-[0-9]{10,13}\-[a-zA-Z0-9\-]*""",
     # "Slack Token V2": r"""xox[baprs]-([0-9a-zA-Z]{10,48})?""",
-    "aws-access-key": r"""((?:AKIA|ABIA|ACCA|ASIA)[0-9A-Z]{16})""",
+    "aws-access-key": r"""\b((?:AKIA|ABIA|ACCA|ASIA)[0-9A-Z]{16})\b""",
     "aws-secret-key": r"""\b([A-Za-z0-9+/]{40})[ \r\n'"\x60]""",
     "azure-key-id": r"""(?i)(%s).{0,20}([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})""",
     "azure-client-secret": r"""\b(?i)(%s).{0,20}([a-z0-9_\.\-~]{34})\b""",
-    "google-api-key": r"""AIza[0-9A-Za-z\-_]{35}\b""",
+    "google-api-key": r"""\bAIza[0-9A-Za-z\-_]{35}\b""",
 }
