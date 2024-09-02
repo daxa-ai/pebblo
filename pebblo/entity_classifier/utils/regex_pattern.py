fe4ec4e931c97dc997e4a55a499c5a5ff7cb27b4
Nishan Jain <gr8nishan@gmail.com>
2024-09-02 22:46:54 +0530

    added changes to add google secret key

--- pebblo/entity_classifier/utils/regex_pattern.py
+++ pebblo/entity_classifier/utils/regex_pattern.py
@@ -14,5 +14,4 @@ regex_secrets_patterns = {
     "azure-key-id": r"""(?i)(%s).{0,20}([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})""",
     "azure-client-secret": r"""\b(?i)(%s).{0,20}([a-z0-9_\.\-~]{34})\b""",
     "google-api-key": r"""\bAIza[0-9A-Za-z\-_]{35}\b""",
-    # "google-key" : r"(?<=BEGIN PRIVATE KEY-----)(.){100,}(?=-----END PRIVATE KEY)"
 }
