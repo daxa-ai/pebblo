ee753d2f7ab2e3e767787bb2b97af54284bc23c2
dristy.cd <dristysrivastava91@gmail.com>
2024-09-04 15:01:05 +0530

    Swagger changes in pebblo server APIs

--- tests/app/test_prompt_api.py
+++ tests/app/test_prompt_api.py
@@ -10,6 +10,33 @@ app = FastAPI()
 app.include_router(router_instance.router)
 
 client = TestClient(app)
+test_payload = {
+    "name": "Test App",
+    "context": [
+        {
+            "retrieved_from": "test_data.pdf",
+            "doc": "Patient SSN: 222-85-4836",
+            "vector_db": "TestDB",
+        },
+        {
+            "retrieved_from": "test_data1.pdf",
+            "doc": "Patient SSN: 222-85-4836",
+            "vector_db": "TestDB",
+        },
+    ],
+    "prompt": {
+        "data": "What is John's SSN",
+        "entities": {},
+        "topics": {},
+        "entityCount": 0,
+        "prompt_gov_enabled": True,
+    },
+    "response": {"data": "Patient SSN is 222-85-4836"},
+    "prompt_time": "2024-04-17T15:03:18.177368",
+    "user": "Test Owner",
+    "user_identities": ["test_group@test.com"],
+    "classifier_location": "local",
+}
 
 
 @pytest.fixture
@@ -25,32 +52,6 @@ def test_app_prompt_success(mock_write_json_to_file):
     Test the app prompt endpoint.
     """
     mock_write_json_to_file.return_value = None
-    test_payload = {
-        "name": "Test App",
-        "context": [
-            {
-                "retrieved_from": "test_data.pdf",
-                "doc": "Patient SSN: 222-85-4836",
-                "vector_db": "TestDB",
-            },
-            {
-                "retrieved_from": "test_data1.pdf",
-                "doc": "Patient SSN: 222-85-4836",
-                "vector_db": "TestDB",
-            },
-        ],
-        "prompt": {
-            "data": "What is John's SSN",
-            "entities": {},
-            "topics": {},
-            "entityCount": 0,
-            "prompt_gov_enabled": True,
-        },
-        "response": {"data": "Patient SSN is 222-85-4836"},
-        "prompt_time": "2024-04-17T15:03:18.177368",
-        "user": "Test Owner",
-        "user_identities": ["test_group@test.com"],
-    }
 
     response = client.post("/v1/prompt", json=test_payload)
     assert response.status_code == 200
@@ -71,35 +72,28 @@ def test_app_prompt_validation_errors(mock_write_json_to_file):
     Test the app prompt endpoint with validation errors.
     """
     mock_write_json_to_file.return_value = None
-    test_payload = {
-        "name": "Test App",
-        "context": [
-            {
-                "retrieved_from": "test_data.pdf",
-                "doc": "This is test doc.",
-            },
-            {
-                "retrieved_from": "test_data1.pdf",
-                "vector_db": "TestDB",
-            },
-        ],
-        "prompt": {
-            "data": "What is Sachin's Passport ID?",
-            "entities": {},
-            "entityCount": 0,
-            "prompt_gov_enabled": True,
+    test_error_payload = test_payload.copy()
+    test_error_payload["context"] = [
+        {
+            "retrieved_from": "test_data.pdf",
+            "doc": "This is test doc.",
         },
-        "response": {"data": "His passport ID is 5484880UA."},
-        "user_identities": ["test_group@test.com"],
-    }
-    response = client.post("/v1/prompt", json=test_payload)
-    assert response.status_code == 400
-    assert response.json()["message"] == (
-        "1 validation error for RetrievalContext\n"
-        "vector_db\n"
-        "  Input should be a valid string [type=string_type, input_value=None, input_type=NoneType]\n"
-        "    For further information visit https://errors.pydantic.dev/2.8/v/string_type"
-    )
+        {
+            "retrieved_from": "test_data1.pdf",
+            "vector_db": "TestDB",
+        },
+    ]
+    response = client.post("/v1/prompt", json=test_error_payload)
+    assert response.status_code == 422
+    assert response.json()["detail"] == [
+        {
+            "type": "missing",
+            "loc": ["body", "context", 0, "vector_db"],
+            "msg": "Field required",
+            "input": {"retrieved_from": "test_data.pdf", "doc": "This is test doc."},
+            "url": "https://errors.pydantic.dev/2.8/v/missing",
+        }
+    ]
 
 
 def test_app_prompt_validation_errors_single_missing_field(mock_write_json_to_file):
@@ -107,35 +101,14 @@ def test_app_prompt_validation_errors_single_missing_field(mock_write_json_to_fi
     Test the app prompt endpoint with validation errors.
     """
     mock_write_json_to_file.return_value = None
-    test_payload = {
-        "name": "Test App",
-        "context": [
-            {
-                "retrieved_from": "test_data.pdf",
-                "doc": "This is test doc.",
-                "vector_db": "TestDB",
-            },
-            {
-                "retrieved_from": "test_data1.pdf",
-                "doc": "This is test1 doc.",
-                "vector_db": "TestDB",
-            },
-        ],
-        "prompt": {
-            "data": "What is Sachin's Passport ID?",
-            "entities": {},
-            "entityCount": 0,
-            "prompt_gov_enabled": True,
-        },
-        "response": {"data": "His passport ID is 5484880UA."},
-        "user": "Test Owner",
-        "user_identities": ["test_group@test.com"],
-    }
-    response = client.post("/v1/prompt", json=test_payload)
-    assert response.status_code == 400
+    test_error_payload = test_payload.copy()
+    test_error_payload.pop("prompt_time")
+    response = client.post("/v1/prompt", json=test_error_payload)
+    assert response.status_code == 422
     assert (
-        "1 validation error for RetrievalData\n" "prompt_time\n"
-    ) in response.json()["message"]
+        "'type': 'missing', 'loc': ['body', 'prompt_time'], 'msg': 'Field required'"
+        in str(response.json()["detail"])
+    )
 
 
 def test_app_prompt_server_error(mock_write_json_to_file):
@@ -143,31 +116,6 @@ def test_app_prompt_server_error(mock_write_json_to_file):
     Test the app prompt endpoint with server error.
     """
     mock_write_json_to_file.side_effect = Exception("Mocked exception")
-    test_payload = {
-        "name": "Test App",
-        "context": [
-            {
-                "retrieved_from": "test_data.pdf",
-                "doc": "This is test doc.",
-                "vector_db": "TestDB",
-            },
-            {
-                "retrieved_from": "test_data1.pdf",
-                "doc": "This is test1 doc.",
-                "vector_db": "TestDB",
-            },
-        ],
-        "prompt": {
-            "data": "What is Sachin's Passport ID?",
-            "entities": {},
-            "entityCount": 0,
-            "prompt_gov_enabled": True,
-        },
-        "response": {"data": "His passport ID is 5484880UA."},
-        "prompt_time": "2024-04-17T15:03:18.177368",
-        "user": "Test Owner",
-        "user_identities": ["test_group@test.com"],
-    }
     response = client.post("/v1/prompt", json=test_payload)
 
     # Assertions
