fe45b47a2f41176b132c76a4388ae744d85f3b02
rutujaac <chaudhari.rutuja99@gmail.com>
2024-09-12 14:29:03 +0530

    fix security issues

--- pebblo/app/pebblo-ui/src/components/snippetDetails.js
+++ pebblo/app/pebblo-ui/src/components/snippetDetails.js
@@ -106,17 +106,13 @@ export function SnippetDetails(props) {
   function onChange(evt) {
     let filteredData;
     if (evt.target.value) {
-      filteredData = snippetList?.filter((item) =>
-        eval(
-          searchField
-            ?.map((sch) =>
-              item[sch]
-                ?.toLocaleLowerCase()
-                ?.includes(evt.target.value.toLocaleLowerCase())
-            )
-            .join(" || ")
-        )
-      );
+      filteredData = snippetList?.filter((item) => {
+        const searchValue = evt.target.value.toLocaleLowerCase();
+        const isMatch = searchField?.some((sch) =>
+          item[sch]?.toLocaleLowerCase()?.includes(searchValue)
+        );
+        return isMatch;
+      });
     } else {
       filteredData = snippetList;
     }
