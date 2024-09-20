f2f1aba66ce85e0067e422ed19b8693f8c9c74a1
Rajendra Kadam <raj.725@outlook.com>
2024-09-20 12:27:36 +0530

    Add owner and size to text metadata

--- pebblo_safeloader/langchain/textloader_postgress/util.py
+++ pebblo_safeloader/langchain/textloader_postgress/util.py
@@ -3,7 +3,7 @@ from typing import Any, Optional
 
 
 def get_data(
-    metadata=False, ids=False, metadatas=False
+        metadata=False, ids=False, metadatas=False
 ) -> tuple[
     list[str],
     Optional[dict[str, Any]],
@@ -40,8 +40,12 @@ def get_data(
     if metadatas:
         # Metadata(source: fake news web url) for each text
         _metadata_list = [
-            {"source": f"https://www.acme.org/news/{i}"}
-            for i in range(1, len(texts) + 1)
+            {
+                "source": f"https://www.acme.org/news/{i + 1}",
+                "owner": "Joe Smith",
+                "size": f"{len(texts[i])}"
+            }
+            for i in range(len(texts))
         ]
     else:
         _metadata_list = None
