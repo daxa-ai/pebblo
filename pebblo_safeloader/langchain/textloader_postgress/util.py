1b0bb3146bf68ebf1aa1ad2f809540862aaa03d3
Rajendra Kadam <raj@Rajendras-MacBook-Pro.local>
2024-09-20 21:16:04 +0530

    Use md5 hashing algorithm, add sensitive data

--- pebblo_safeloader/langchain/textloader_postgress/util.py
+++ pebblo_safeloader/langchain/textloader_postgress/util.py
@@ -1,4 +1,4 @@
-from hashlib import sha256
+from hashlib import  md5
 from typing import Any, Optional
 
 
@@ -30,6 +30,7 @@ def get_data(
         "Roberts reminded the board of the scheduled retreat coming up in three months, and provided a drafted retreat schedule. The board provided feedback on the agenda and the consensus was that, outside of making a few minor changes, the committee should move forward as planned. No board action required.",
         "Claims: An adaptive pacing system for implantable cardiac devices, comprising a pulse generator, multiple sensing electrodes, a microprocessor-based control unit, a wireless communication module, and memory for dynamically adjusting pacing parameters based on real-time physiological data.  The system of claim 1, wherein the adaptive pacing algorithms include rate-responsive pacing based on physical activity.  The system of claim 1, further comprising an external monitoring system for remote data access and modification of pacing parameters.",
         "Sachin's SSN is 222-85-4836. His passport ID is 5484880UA.  Sachin's driver's license number is S9998888.  Sachin's bank account number is 70048841700216300.  His American express credit card number is 371449635398431.  His UK IBAN Code is AZ96AZEJ00000000001234567890.  ITIN number 993-77 0690.",
+        "Github Token is: ghp_hgu657yiujgwfrtigu3ver238765tyuhygvtrder6t7gyvhbuy5e676578976tyghy76578uygfyfgcyturtdf, AWS Access Key is: AKIAQIPT4PDORIRTV6PH  AWS Secret Key is : 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",
     ]
 
     if metadata:
@@ -47,8 +48,8 @@ def get_data(
         _metadata_list = None
 
     if ids:
-        # Unique ids for each text (sha256 hash of text)
-        _ids = [sha256(text.encode()).hexdigest() for text in texts]
+        # Unique ids for each text (md5 hash of text)
+        _ids = [md5(text.encode()).hexdigest() for text in texts]
     else:
         _ids = None
     return texts, _metadata, _metadata_list, _ids
